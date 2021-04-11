67fb62a68b51447b3cdc75bcb753769af8bdbfb7
stephym <60659457+stephymelo@users.noreply.github.com>
2021-04-11 17:32:01 -0500

    forms

---
+++ script/productForm.js
@@ -0,0 +1,37 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBgeF_BrTBqeSOLHlAtxNd_aDxW9wHDkps",
+    authDomain: "web2021-68d94.firebaseapp.com",
+    projectId: "web2021-68d94",
+    storageBucket: "web2021-68d94.appspot.com",
+    messagingSenderId: "434084723620",
+    appId: "1:434084723620:web:c624db7e0da45d130e9f31"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();  
+const productForm = document.querySelector('.productForm');
+
+productForm.addEventListener('submit',function(event){
+    event.preventDefault();
+    console.log('name: ',productForm.name.value);
+    console.log('price: ',productForm.price.value);
+    console.log('vinyl: ',productForm.format_vinyl.checked);
+    console.log('cd: ',productForm.format_cd.checked);
+    console.log('digital: ',productForm.format_digital.checked);
+
+    const product = {
+        name:productForm.name.value,
+        price:parseInt(productForm.price.value),
+        format: [],
+    };
+    if(productForm.format_vinyl.checked) product.format.push('vinyl');
+    if(productForm.format_cd.checked) product.format.push('cd');
+    if(productForm.format_digital.checked) product.format.push('digital');
+
+    db.collection('productos').add(product).then(function(docRef){
+        console.log(docRef.id);
+
+    });
+
+
+});
