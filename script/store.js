61de8955cd749a8ce4aee04f52a46fca2d17957a
stephym <60659457+stephymelo@users.noreply.github.com>
2021-04-21 18:40:04 -0500

    hu

--- script/store.js
+++ script/store.js
@@ -2,10 +2,14 @@ const list = document.querySelector('.list');
 
 const handleCollectionResult = (querySnapshot) => {
   list.innerHTML = '';
+
+
+  console.log(querySnapshot.length);
   querySnapshot.forEach((doc) => {
       const data = doc.data();
       const product = document.createElement('a');
       let img = data.images[0]?.url;
+      
     //   if(!img) {
     //     img = './images/placeholder-image.png';
     //   }
@@ -13,7 +17,7 @@ const handleCollectionResult = (querySnapshot) => {
         <img class="product__img" src="${img}" alt="">
         <div class="product__info">
           <h1 class="product__title">
-            (${data.type}) ${data.name}
+            (${data.name}) ${data.format}
           </h1>
           <h3 class="product__price">$ ${data.price}</h3>
         </div>
@@ -28,46 +32,48 @@ const handleCollectionResult = (querySnapshot) => {
 const filters = document.querySelector('.filters');
 
 filters.addEventListener('change', function () {
-  console.log(filters.test);
+  
+  let productsCollection = db.collection('products');
 
-//   let productsCollection = db.collection('products');
 
   const types = [];
-  filters.test.forEach(function (checkbox) {
+  //checkboxitos
+  filters.type.forEach(function (checkbox) {
     if(checkbox.checked) {
       types.push(checkbox.getAttribute('data-type'));
     }
   });
   if(types.length > 0){
-    productsCol = productsCol.where('type', 'in', types);
+    console.log(productsCollection.get());
+    productsCollection = productsCollection.where('format', 'in', types);
   }
 
   if(filters.type.value) {
-    productsCol = productsCol.where('type', '==', filters.type.value);
+    productsCollection = productsCollection.where('format', '==', filters.type.value);
   }
 
-  if(filters.price.value) {
-    switch(filters.price.value) {
-      case '0':
-        productsCol = productsCol.where('price', '<', 10);
-        break;
-      case '1':
-        productsCol = productsCol.where('price', '>=', 10).where('price', '<', 30);
-        break;
-      case '2':
-        productsCol = productsCol.where('price', '>=', 30);
-        break;
-    }
-  }
+  // if(filters.price.value) {
+  //   switch(filters.price.value) {
+  //     case '0':
+  //       productsCollection = productsCollection.where('price', '<', 10);
+  //       break;
+  //     case '1':
+  //       productsCollection = productsCollection.where('price', '>=', 10).where('price', '<', 30);
+  //       break;
+  //     case '2':
+  //       productsCollection = productsCollection.where('price', '>=', 30);
+  //       break;
+  //   }
+  // }
 
-  productsCol.get().then(handleCollectionResult);
+  productsCollection.get().then(handleCollectionResult);
 });
 
-let productsCol = db.collection('products');
+let productsCollection = db.collection('products');
 
 const params = new URLSearchParams(location.search);
 if(params.get('type')){
-  productsCol = productsCol.where('type', '==', params.get('type'));
+  productsCollection = productsCollection.where('type', '==', params.get('type'));
 }
 
-productsCol.get().then(handleCollectionResult)
+productsCollection.get().then(handleCollectionResult);
